6c0d282c4f155443ecee68e93f781f45babbb80b
thinhvo18 <thinhvq18112002@gmail.com>
2024-02-27 23:30:26 +0700

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
 import './App.scss';
+import Login from './components/Login/Login';
+import Nav from './components/Navigation/Nav';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello world React with Hoi Dan IT</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <div className="app-container">
+                {/* <Nav /> */}
+                <Switch>
+                    <Route path="/about">About</Route>
+                    <Route path="/news">News</Route>
+                    <Route path="/contact">Contact</Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/" exact>
+                        Home
+                    </Route>
+                    <Route path="*">404 Not Found</Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
